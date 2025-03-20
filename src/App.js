417d9747ca8b117ccefa41417bb730585ee59fc6
dhanush <dhanushshetty9741@gmail.com>
2025-03-20 16:43:53 +0530

    sceond commint

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-
+import './App.css';
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-      <Footer/>
-    </Router>
+    <div className="app-container">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import Banner from '../components/Banner'
-import About from './About'
-import Plans from './Plans'
-import Levels from './Levels'
-import ContactUs from './ContactUs'
+import React from 'react';
+import Banner from '../components/Banner';
+import About from './About';
+import Plans from './Plans';
+import Levels from './Levels';
+import ContactUs from './ContactUs';
+
 function Home() {
   return (
-    <div>
-      <Banner/>
-      <About/>
-      <Plans/>
-      <Levels/>
-      <ContactUs/>
+    <div className="home-container">
+      <Banner />
+      <About />
+      <Plans />
+      <Levels />
+      <ContactUs />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
